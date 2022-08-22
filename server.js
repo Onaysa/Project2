8539531efb23af78b2b2eb4527835c8b8c46cd9c
Asimone13 <alecsimone2@gmail.com>
2022-08-22 19:16:06 -0400

    updates to run properly

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
@@ -29,7 +28,7 @@ const sess = {
 app.use(session(sess));
 
 // Setting up handlebars
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
